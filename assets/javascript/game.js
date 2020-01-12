8c1338f06f77e222b15ed78d6f1af8e56f87b533
beardykraken <vcasmirri@gmail.com>
2020-01-12 14:47:01 -0500

    Added else if condition for decreasing guess counter, the loss condition, and an else condition for users not inputting letters.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,5 @@
+// Initial variables & starting values
+
 var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 var wins = document.getElementById("wins");
 var losses = document.getElementById("losses");
@@ -7,9 +9,13 @@ var winCounter = 0;
 var lossCounter = 0;
 var guessCounter = 9;
 
+// Triggers game on key up and stores user's and computer's guesses
+
 document.onkeyup = function(event) {
 var userGuess = event.key.toUpperCase();
 var computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
+
+// Defines parameters for winning, increases win counter, resets Guesses Left/Made
 if ((userGuess === computerGuess) && (event.keyCode >= 65 && event.keyCode <= 90)) {
     winCounter++;
     wins.textContent = "Wins: " + winCounter;
@@ -18,7 +24,26 @@ if ((userGuess === computerGuess) && (event.keyCode >= 65 && event.keyCode <= 90
     guessesMade.textContent = "Guesses Made:";
 }
 
-
-
-
+// Defines parameters for losing, increases loss counter, reduces Guesses Left, adds to Guesses Made
+else if (event.keyCode >= 65 && event.keyCode <= 90) {
+    guessCounter--;
+    guessesLeft.textContent = "Guesses Left: " + guessCounter;
+    guessesMade.textContent += userGuess;
+    //code for storing guesses made here
+    if (guessCounter === 0) {
+        lossCounter++;
+        losses.textContent = "Losses: " + lossCounter;
+        guessCounter = 9;
+        guessesLeft.textContent = "Guesses Left: 9";
+        guessesMade.textContent = "Guesses Made:";
+    }
 }
+
+else {
+    alert("That's not a letter!");
+}
+}
+
+
+
+// console.log("U: " + userGuess + " C: " + computerGuess ". You Won!");
